05fb3a717cac189ae832a93c53a3cd74aeb0f271
tbrustggc <tbrust@ggc.edu>
2017-11-18 06:59:23 -0500

    trying to get rid of some conflicts

--- WinRemote/app/src/main/java/com/disabledtech/winremote/view/MainActivity.java
+++ WinRemote/app/src/main/java/com/disabledtech/winremote/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.disabledtech.winremote.view;
 
 import android.Manifest;
 import android.bluetooth.BluetoothSocket;
+<<<<<<< HEAD
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -10,6 +11,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+=======
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+>>>>>>> development_integration
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,16 +25,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+<<<<<<< HEAD
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+=======
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+>>>>>>> development_integration
 
 import com.disabledtech.winremote.R;
 import com.disabledtech.winremote.control.BTConnectionClient;
 import com.disabledtech.winremote.control.BTDataIO;
+<<<<<<< HEAD
+=======
+import com.disabledtech.winremote.exceptions.ServerConnectionClosedException;
+>>>>>>> development_integration
 import com.disabledtech.winremote.interfaces.IServerConnectionListener;
 import com.disabledtech.winremote.model.WinAction;
 import com.disabledtech.winremote.model.WinActionButton;
@@ -35,6 +52,7 @@ import com.disabledtech.winremote.utils.Debug;
 import com.disabledtech.winremote.utils.Device;
 
 import java.io.IOException;
+<<<<<<< HEAD
 import java.util.LinkedList;
 import java.util.List;
 
@@ -79,6 +97,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	}
 
 
+=======
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+		implements NavigationView.OnNavigationItemSelectedListener, IServerConnectionListener, View.OnClickListener
+{
+
+	private static final int REQUEST_ACCESS_COARSE_LOCATION = 1; // used to identify permission requests
+
+	private BTConnectionClient m_ConnectionClient;
+	private BTDataIO m_DataIO;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		initializeLayout();
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+
+		m_DataIO.closeConnection();
+	}
+
+	/**
+	 * Called when a component of the UI is pressed.
+	 *
+	 * @param view
+	 */
+>>>>>>> development_integration
 	public void onClick(View view)
 	{
 		if (view instanceof WinActionButton)
@@ -87,6 +139,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 			return;
 		}
 
+<<<<<<< HEAD
 		
 
 		Debug.logError("UI view click not implemented!");
@@ -96,11 +149,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	}
 
+=======
+		Debug.logError("UI view click not implemented!");
+	}
+
+>>>>>>> development_integration
 	private void handleWinButtonPressed(WinActionButton buttonPressed)
 	{
 		try
 		{
 			m_DataIO.send(buttonPressed.getWinAction());
+<<<<<<< HEAD
 		} catch (IOException e)
 		{
 			// TODO UI feedback
@@ -108,11 +167,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 			e.printStackTrace();
 		}
 
+=======
+		}
+		catch(ServerConnectionClosedException scce)
+		{
+			// TODO better UI handling
+			Device.showToast(this, "Server connection lost.");
+			clearButtons();
+		}
+		catch (IOException ioe)
+		{
+			// TODO UI feedback
+			Debug.logError("Error sending button information to server.");
+			ioe.printStackTrace();
+		}
+>>>>>>> development_integration
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
+<<<<<<< HEAD
 
 		int id = item.getItemId();
 
@@ -123,11 +198,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 				Device.showToast(this, "Attempting connection...");
 				beginServerConnection();
 				return true;
+=======
+		int id = item.getItemId();
+
+		switch (id)
+		{
+			case R.id.action_connect:
+				// TODO user feedback while connection to server is being established, have this perform automatically on launch
+				Device.showToast(this, "Attempting connection...");
+				beginServerConnection();
+				return true;
+
+			case R.id.action_disconnect:
+				m_DataIO.closeConnection();
+				clearButtons();
+				return true;
+>>>>>>> development_integration
 		}
 
 		return super.onOptionsItemSelected(item);
 	}
 
+<<<<<<< HEAD
+=======
+	/**
+	 * Clears all the buttons from the grid layout.
+	 */
+	private void clearButtons()
+	{
+		ViewGroup gridButtonGroup = (ViewGroup) findViewById(R.id.grid_layout);
+		gridButtonGroup.removeAllViews();
+	}
+
+>>>>>>> development_integration
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item)
 	{
@@ -148,7 +251,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	@Override
 	public void onBackPressed()
 	{
+<<<<<<< HEAD
 
+=======
+>>>>>>> development_integration
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 		// if the template drawer is open, back should just close it
@@ -168,7 +274,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	 */
 	private void beginServerConnection()
 	{
+<<<<<<< HEAD
 
+=======
+>>>>>>> development_integration
 		if (Device.hasBluetoothPermissions(this))
 		{
 			getServerConnection();
@@ -187,13 +296,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	 */
 	public void getServerConnection()
 	{
+<<<<<<< HEAD
 
+=======
+>>>>>>> development_integration
 		if (m_ConnectionClient == null)
 		{
 			m_ConnectionClient = new BTConnectionClient(this, this);
 		}
 
+<<<<<<< HEAD
 		m_ConnectionClient.connectToServer();
+=======
+		m_ConnectionClient.attemptBondedConnection();
+>>>>>>> development_integration
 	}
 
 	/**
@@ -207,7 +323,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	@Override
 	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
 	{
+<<<<<<< HEAD
 
+=======
+>>>>>>> development_integration
 		boolean permissionGranted = grantResults.length > 0 &&
 				grantResults[0] == PackageManager.PERMISSION_GRANTED;
 
@@ -228,7 +347,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	@Override
 	public void serverConnected(BluetoothSocket connectedSocket)
 	{
+<<<<<<< HEAD
 
+=======
+>>>>>>> development_integration
 		Device.showToast(this, "Connection to the server made!");
 		m_DataIO = new BTDataIO(connectedSocket);
 
@@ -244,11 +366,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	 */
 	private void populateActivityButtons(List<WinAction> userActions)
 	{
+<<<<<<< HEAD
 		LinearLayout layout = (LinearLayout) findViewById(R.id.linear_layout);
+=======
+		GridLayout layout = (GridLayout) findViewById(R.id.grid_layout);
+>>>>>>> development_integration
 
 		// TODO pretty buttons
 		for (WinAction action : userActions)
 		{
+<<<<<<< HEAD
 			final WinActionButton actionButton = new WinActionButton(this, action);
 			actionButton.setOnClickListener(this);
 			layout.addView(actionButton);
@@ -267,6 +394,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 					return true;
 				}
 			});
+=======
+			WinActionButton actionButton = new WinActionButton(this, action);
+			actionButton.setOnClickListener(this);
+			layout.addView(actionButton);
+>>>>>>> development_integration
 		}
 	}
 
@@ -286,7 +418,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	private void initializeLayout()
 	{
+<<<<<<< HEAD
 
+=======
+>>>>>>> development_integration
 		setContentView(R.layout.activity_main);
 
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -309,6 +444,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		getMenuInflater().inflate(R.menu.main_interface, menu);
 		return true;
 	}
+<<<<<<< HEAD
 
 	public void onWinActionButtonClick(View v) {
 		WinActionButton actionButton = (WinActionButton) v;
@@ -316,4 +452,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	}
 
+=======
+>>>>>>> development_integration
 }
